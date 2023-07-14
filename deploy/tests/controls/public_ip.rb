3a59d4f589dce73160d6eb31525760ceb20362e3
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-07-14 18:40:48 +0100

    Skip IP address testing
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/tests/controls/public_ip.rb
+++ deploy/tests/controls/public_ip.rb
@@ -2,6 +2,10 @@ control "azure-kubernetes-public-ip" do
     title "App Gateway IP"
     desc "Ensure that a Public IP address has been configured for the App Gateway"
 
+    only_if("IP address should be tested") do
+        false
+    end
+
     describe azure_public_ip(resource_group: input("resource_group_name"), name: input("app_gateway_ip")) do
         it { should exist }
         its("location") { should cmp input("region") }
