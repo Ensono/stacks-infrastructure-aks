43976e59fe93fd2a7317bd3994b66bacc237d45d
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-07-17 14:22:25 +0100

    Updated plan file names
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ build/scripts/Set-TFVars.ps1
@@ -0,0 +1,31 @@
+
+[CmdletBinding()]
+param (
+
+    [string]
+    # Prefix to look for in enviornment variables
+    $prefix = "TF_VAR_*"
+)
+
+$tfvars =  [System.Text.StringBuilder]::new()
+
+
+# Output the values of the enviornment variables
+Get-ChildItem -Path env: | Where-Object name -like $prefix | % {
+
+    # Get th name of the variable, without the prefix
+    $name = $_.name -replace $prefix,""
+
+    # set the value
+    $value = $_.value -replace "\`"", "\`""
+
+    if (!($value -is [int])) {
+        $value = "`"{0}`"" -f $value
+    }
+
+    $line = '{0} = {1}' -f $name, $value
+    [void]$tfvars.AppendLine($line)
+
+}
+
+$tfvars.ToString()
